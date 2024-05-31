67088084ff6c0b5e7daa8536e3edf95bfbb9dd2a
Guillaume <guillaume.rebourgeon@hotmail.fr>
2024-05-31 14:29:57 +0200

    t

--- index.php
+++ index.php
@@ -1,4 +1,21 @@
 <?php
-var_dump($_GET);
+// var_dump($_GET);
 echo "test";
+
+// on sépare les paramètres
+$params = explode('/', $_GET['p']);
+var_dump($params);
+
+// est ce qu'un parametre existe
+
+if ($params[0] != ""){
+    $controller = ucfirst($params[0]);
+    echo $controller;
+
+}else{
+
+}
+
+
+
 ?>
